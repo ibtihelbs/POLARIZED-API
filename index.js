9f8e825a8b38b900323c2d14802c3411a1106989
ibtihel ben salah <91198460+ibtihelbs@users.noreply.github.com>
2024-10-02 11:26:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:3000",
       "https://polarized-store.onrender.com",
       "https://polarized-admin.onrender.com",
+      "http://localhost:5173"
     ],
   })
 );
